08bd8556488a507eff5f2918ba1f2cb9297b9703
Richard South <southy98@gmail.com>
2020-08-22 13:35:51 +0100

    Initial commit

---
+++ src/resource.h
@@ -0,0 +1,12 @@
+#define IDC_STATIC -1
+#define IDD_MAIN                        101
+#define IDC_TEXT                        1000
+#define IDC_INTERVAL                    1001
+#define IDC_LIST                        1002
+#define IDC_ADD                         1003
+#define IDC_CLEAR                       1004
+#define IDC_REMOVE                      1005
+#define IDC_SHOWCOUNT                   1006
+#define IDC_START                       1007
+#define IDT_TYPINGTIMER                 1008
+#define IDC_DELAY                       1009
